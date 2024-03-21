f5a79f5105579551b64a93830e1de137a88ce7be
cskaandorp <casper@compunist.nl>
2024-03-21 09:57:27 +0100

    It's working, thank you Jonathan de Bruin, aka the-man-with-the-plan

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create tables
-asreview auth-tool create-db --db-uri=${ASREVIEW_LAB_SQLALCHEMY_DATABASE_URI}
-
-# start gunicorn
-gunicorn -w ${WORKERS} -b "0.0.0.0:5006" "asreview.webapp.app:create_app()"
